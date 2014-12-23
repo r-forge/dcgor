f62b241b7a557eae84ed4689bfc248681a65a7a7
hfang (Dr ) <hfang.shanghai@gmail.com>
2014-12-23 16:06:04 +0000

    Update to the version 1.0.5
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dcgor@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 975cd04f9c753cd209175473d6222b7ecc0a2d8c

--- pkg/R/dcRWRpredict.r
+++ pkg/R/dcRWRpredict.r
@@ -1,6 +1,6 @@
-#' Function to perform RWR-based ontology term predictions from an input known annotations and an input graph
+#' Function to perform RWR-based ontology term predictions from input known annotations and an input graph
 #'
-#' \code{dcRWRpredict} is supposed to perform ontology term predictions based on Random Walk with Restart (RWR) from an input known annotations and an input graph.
+#' \code{dcRWRpredict} is supposed to perform ontology term predictions based on Random Walk with Restart (RWR) from input known annotations and an input graph.
 #'
 #' @param data an input gene-term data matrix containing known annotations used for seeds. Each value in input matrix does not necessarily have to be binary (non-zeros will be used as a weight, but should be non-negative for easy interpretation). Also, data can be a list, each containing the known annotated genes
 #' @param g an object of class "igraph" or \code{\link{Dnetwork}}
@@ -11,6 +11,7 @@
 #' @param restart the restart probability used for RWR. The restart probability takes the value from 0 to 1, controlling the range from the starting nodes/seeds that the walker will explore. The higher the value, the more likely the walker is to visit the nodes centered on the starting nodes. At the extreme when the restart probability is zero, the walker moves freely to the neighbors at each step without restarting from seeds, i.e., following a random walk (RW)
 #' @param normalise.affinity.matrix the way to normalise the output affinity matrix. It can be 'none' for no normalisation, 'quantile' for quantile normalisation to ensure that columns (if multiple) of the output affinity matrix have the same quantiles
 #' @param leave.one.out logical to indicate whether the leave-one-out test is used for predictions. By default, it sets to true for doing leave-one-out test (that is, known seeds are removed)
+#' @param propagation how to propagate the score. It can be "max" for retaining the maximum score (by default), "sum" for additively accumulating the score
 #' @param scale.method the method used to scale the predictive scores. It can be: "none" for no scaling, "linear" for being linearily scaled into the range between 0 and 1, "log" for the same as "linear" but being first log-transformed before being scaled. The scaling between 0 and 1 is done via: \eqn{\frac{S - S_{min}}{S_{max} - S_{min}}}, where \eqn{S_{min}} and \eqn{S_{max}} are the minimum and maximum values for \eqn{S}
 #' @param parallel logical to indicate whether parallel computation with multicores is used. By default, it sets to true, but not necessarily does so. Partly because parallel backends available will be system-specific (now only Linux or Mac OS). Also, it will depend on whether these two packages "foreach" and "doMC" have been installed. It can be installed via: \code{source("http://bioconductor.org/biocLite.R"); biocLite(c("foreach","doMC"))}. If not yet installed, this option will be disabled
 #' @param multicores an integer to specify how many cores will be registered as the multicore parallel backend to the 'foreach' package. If NULL, it will use a half of cores available in a user's computer. This option only works when parallel computation is enabled
@@ -64,7 +65,7 @@
 #' plot(res_PR[,2], res_PR[,1], xlim=c(0,1), ylim=c(0,1), type="b", xlab="Recall", ylab="Precision")
 #' }
 
-dcRWRpredict <- function(data, g, output.file=NULL, ontology=c(NA,"GOBP","GOMF","GOCC","DO","HPPA","HPMI","HPON","MP","EC","KW","UP"), method=c("indirect","direct"), normalise=c("laplacian","row","column","none"), restart=0.75, normalise.affinity.matrix=c("none","quantile"), leave.one.out=T, scale.method=c("log","linear","none"), parallel=TRUE, multicores=NULL, verbose=T, RData.ontology.customised=NULL, RData.location="http://dcgor.r-forge.r-project.org/data")
+dcRWRpredict <- function(data, g, output.file=NULL, ontology=c(NA,"GOBP","GOMF","GOCC","DO","HPPA","HPMI","HPON","MP","EC","KW","UP"), method=c("indirect","direct"), normalise=c("laplacian","row","column","none"), restart=0.75, normalise.affinity.matrix=c("none","quantile"), leave.one.out=T, propagation=c("max","sum"), scale.method=c("log","linear","none"), parallel=TRUE, multicores=NULL, verbose=T, RData.ontology.customised=NULL, RData.location="http://dcgor.r-forge.r-project.org/data")
 {
 
     startT <- Sys.time()
@@ -136,10 +137,14 @@ dcRWRpredict <- function(data, g, output.file=NULL, ontology=c(NA,"GOBP","GOMF",
     }
     
     if(verbose){
-        message(sprintf("\tusing '%s' method to do RWR (%s)...", method, as.character(Sys.time())), appendLF=T)
+        if(leave.one.out){
+            message(sprintf("\tusing '%s' method to do RWR with leave-one-out test (%s)...", method, as.character(Sys.time())), appendLF=T)
+        }else{
+            message(sprintf("\tusing '%s' method to do RWR without leave-one-out test (%s)...", method, as.character(Sys.time())), appendLF=T)
+        }
         
         message(paste(c("\n##############################"), collapse=""), appendLF=T)
-        message(paste(c("'dnet::dRWR' is called"), collapse=""), appendLF=T)
+        message(paste(c("'dnet::dRWR' is being called..."), collapse=""), appendLF=T)
         message(paste(c("##############################\n"), collapse=""), appendLF=T)
         
     }
@@ -159,7 +164,7 @@ dcRWRpredict <- function(data, g, output.file=NULL, ontology=c(NA,"GOBP","GOMF",
             col_sum <- Matrix::colSums(P0matrix)
             col_sum_matrix <- matrix(rep(col_sum,nrow(ASmatrix)), ncol=ncol(ASmatrix), nrow=nrow(ASmatrix), byrow=T)
             ##################
-            ### for seeds, the average should be 1 less
+            ### for seeds, the average should be 1-less
             col_sum_adj_matrix <- col_sum_matrix - as.matrix(P0matrix)
             AAmatrix <- ASmatrix / col_sum_adj_matrix
             ##################
@@ -191,7 +196,7 @@ dcRWRpredict <- function(data, g, output.file=NULL, ontology=c(NA,"GOBP","GOMF",
     ####################################################
     if(verbose){
         message(paste(c("##############################"), collapse=""), appendLF=T)
-        message(paste(c("'dnet::dRWR' is completed"), collapse=""), appendLF=T)
+        message(paste(c("'dnet::dRWR' has been completed!"), collapse=""), appendLF=T)
         message(paste(c("##############################\n"), collapse=""), appendLF=T)
     
         message(sprintf("Second, propagate '%s' ontology annotations (%s)...", ontology, as.character(Sys.time())), appendLF=T)
@@ -204,16 +209,16 @@ dcRWRpredict <- function(data, g, output.file=NULL, ontology=c(NA,"GOBP","GOMF",
     
     if(verbose){
         message(paste(c("\n##############################"), collapse=""), appendLF=T)
-        message(paste(c("'dcAlgoPropagate' is called"), collapse=""), appendLF=T)
+        message(paste(c("'dcAlgoPropagate' is being called..."), collapse=""), appendLF=T)
         message(paste(c("##############################\n"), collapse=""), appendLF=T)
     }
     
-    res_RData <- suppressMessages(dcAlgoPropagate(input.file=output, ontology=ontology, output.file=NA, verbose=verbose, RData.ontology.customised=RData.ontology.customised, RData.location=RData.location))
+    res_RData <- dcAlgoPropagate(input.file=output, ontology=ontology, propagation=propagation, output.file=NA, verbose=verbose, RData.ontology.customised=RData.ontology.customised, RData.location=RData.location)
     score <- res_RData$hscore
     
     if(verbose){
         message(paste(c("##############################"), collapse=""), appendLF=T)
-        message(paste(c("'dcAlgoPropagate' is completed"), collapse=""), appendLF=T)
+        message(paste(c("'dcAlgoPropagate' has been completed!"), collapse=""), appendLF=T)
         message(paste(c("##############################\n"), collapse=""), appendLF=T)
     
         message(sprintf("Third, rescale predictive score using '%s' method (%s)...", scale.method, as.character(Sys.time())), appendLF=T)
